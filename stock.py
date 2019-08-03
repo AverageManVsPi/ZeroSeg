8e6846fd1801b1b375cb625b5becddd0587e0ed1
Jakub Białoskórski <bialoskorski@icloud.com>
2019-08-03 18:49:48 +0200

    added brightness button steering

--- stock.py
+++ stock.py
@@ -6,10 +6,23 @@ import requests
 import alpha_vantage
 import json
 import urllib
+import RPi.GPIO as GPIO
 
+# setup buttons
+button1 = 17
+button2 = 26
+GPIO.setmode(GPIO.BCM) # Use BCM GPIO numbers
+GPIO.setup(button1, GPIO.IN)
+GPIO.setup(button2, GPIO.IN)
+
+# cleanup before run
 device = led.sevensegment(cascaded=2)
 device.clear()
 
+# set brightness to minimal
+level = 1
+
+# check Zendesk stock price
 def stock():
     API_URL = "https://www.alphavantage.co/query"
     symbols = ['ZEN']
@@ -23,5 +36,25 @@ def stock():
         output = data["Global Quote"]["05. price"]
         output_filtered = output[:4]
         device.write_text(1, "ZEN "+ output_filtered)
+        time.sleep(300)
 
-stock()
+while True:
+    device.brightness(level)
+    if not GPIO.input(button1):
+        if level == 1:
+            print "MIN"
+            time.sleep(0.5)
+        if level >= 2:
+            level = level -1
+            print "-1 ", level
+            time.sleep(0.5)
+
+    elif not GPIO.input(button2):
+        if level == 15:
+            print "MAX"
+            time.sleep(0.5)
+        if level <= 14:
+            level = level +1
+            print "+1 ", level
+            time.sleep(0.5)
+    stock()
